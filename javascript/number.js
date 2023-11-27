ec755db949d39259b320e0a6621ece0b02b0a911
piyush-0133 <53461278+piyush-0133@users.noreply.github.com>
2023-11-27 16:12:53 +0000

    math

---
+++ javascript/number.js
@@ -0,0 +1,46 @@
+/*
+const num0 = 100
+console.log(num0)
+
+const num2 = new Number(239)
+const num3 = num2.toString()
+console.log(typeof num3)
+
+console.log(num3.length)
+
+const numFour = 127.877
+
+console.log(numFour.toFixed(2))
+console.log(numFour.toPrecision(4))
+
+console.log(numFour.toPrecision(3))
+
+const amount = 1000000
+console.log(amount.toLocaleString())
+console.log(amount.toLocaleString('en-IN'))
+
+
+//=======================================================================//
+
+/*console.log(Math)
+console.log(Math.abs(-9))
+console.log(Math.abs(9))
+
+console.log(Math.round(6.6))
+console.log(Math.round(6.5))
+console.log(Math.ceil(6.1))
+console.log(Math.floor(6.9))
+*/
+
+console.log(Math.random())
+console.log(Math.random()*10) // itm may contain zero so add 1
+
+console.log((Math.random()*10) + 1)
+
+const min = 10
+const max= 20
+
+console.log(Math.floor(Math.random()* (max - min + 1)) + min)
+
+
+
--- javascript/string.js
+++ javascript/string.js
@@ -3,3 +3,21 @@ const name = "Adarsh"
 let age = 34
 
 console.log(`Hi! This is ${name} and my age is ${age} years`)
+
+const gameName = new String('Piyush')
+console.log(gameName.indexOf('i'))
+console.log(gameName.charAt(0))
+console.log(gameName.charAt('1')) // ignore string in number
+console.log(gameName.toUpperCase() );console.log(gameName.length)
+
+const changeName = new String(gameName.substring(0,5))//not allow negative value output same
+console.log(changeName)
+
+const cutName = new String(changeName.slice(-4,4))
+
+console.log(cutName)
+
+const stringTwo = "           http://piyush&*(34singh"
+console.log(stringTwo)
+console.log(stringTwo.trim())
+console.log(stringTwo.replace('&*(34', '-'))
